9957913edefe0126fb974470559386ed587f3010
Andrey Shvets <sas@vava.cars>
2022-05-30 03:57:01 +0300

    Remove extra dependencies from WebAppTools

--- tests/ServicesTestFramework.DatabaseContainers.Tests/Helpers/TestContainerHelpers.cs
+++ tests/ServicesTestFramework.DatabaseContainers.Tests/Helpers/TestContainerHelpers.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
-using ServicesTestFramework.DatabaseContainers.Tests.Controllers;
+﻿using ServicesTestFramework.DatabaseContainers.Tests.Controllers;
 using ServicesTestFramework.ExampleApi;
 using ServicesTestFramework.WebAppTools.Extensions;
 
@@ -10,19 +6,10 @@ namespace ServicesTestFramework.DatabaseContainers.Tests.Helpers
 {
     public static class TestContainerHelpers
     {
-        public static string MountSourceFolder => $"mysqlData-{Guid.NewGuid()}";
-
         public static IDatabaseController StartWebAppWithClient(string connectionString)
         {
-            var imMemoryConfig = new Dictionary<string, string>
-            {
-                { "Database:ConnectionString", connectionString }
-            };
-
-            var factory = new WebApplicationFactory<Startup>();
-
-            var client = factory.WithWebHostConfiguration(
-                    configureAppConfiguration: configBuilder => configBuilder.AddInMemoryCollection(imMemoryConfig))
+            var client = new WebApplicationBuilder<Startup>()
+                .AddConfiguration("Database:ConnectionString", connectionString)
                 .CreateClient();
 
             var databaseClient = client.ClientFor<IDatabaseController>();
